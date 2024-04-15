f9f4186fd0679a8c14969e8260cdadd3d04556ed
alex-yang-upenn <chukyang@sas.upenn.edu>
2024-04-15 01:42:49 -0400

    attempting stream fix

--- kernel_wrapper.h
+++ kernel_wrapper.h
@@ -8,7 +8,7 @@
 
 #define BATCHSIZE 8192
 
-#define DATA_SIZE_IN (1 * N_INPUT_1_1 * N_INPUT_2_1)
+#define DATA_SIZE_IN (N_INPUT_1_1 * N_INPUT_2_1)
 #define DEPTH N_INPUT_3_1
 
 #define INSTREAMSIZE (BATCHSIZE * DATA_SIZE_IN * DEPTH)
@@ -18,7 +18,7 @@
 typedef ap_fixed<16,6> in_buffer_t;
 typedef ap_fixed<16,6> out_buffer_t;
 
-typedef input_t in_stream_t
-typedef result_t out_stream_t
+typedef nnet::array<ap_fixed<16,6>, 3*1> in_stream_t
+typedef nnet::array<ap_fixed<16,6>, 10*1> out_stream_t
 
 #endif
