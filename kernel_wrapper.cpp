8cae9649fbf13564ad3cf50c3f5313662615663e
alex-yang-upenn <chukyang@sas.upenn.edu>
2024-04-15 01:15:30 -0400

    another try at fixing stream

--- kernel_wrapper.cpp
+++ kernel_wrapper.cpp
@@ -1,10 +1,10 @@
 #include "kernel_wrapper.h"
 #include "firmware/myproject.h"
 
-static void read_input(const in_buffer_t *in, hls::stream<input_t> &input, int n) {
+static void read_input(const in_buffer_t *in, hls::stream<in_stream_t> &input, int n) {
   for (int i = 0; i < DATA_SIZE_IN; i++) {
     #pragma HLS PIPELINE
-    input_t tmp;
+    in_stream_t tmp;
     for (int j = 0; j < DEPTH; j++) {
       #pragma HLS UNROLL
       tmp[j] = in[(n * DATA_SIZE_IN * DEPTH) + (i * DEPTH) + j];
@@ -13,8 +13,8 @@ static void read_input(const in_buffer_t *in, hls::stream<input_t> &input, int n
   }
 }
 
-static void write_result(out_buffer_t *out, hls::stream<result_t> &output, int n) {
-  result_t tmp = output.read();
+static void write_result(out_buffer_t *out, hls::stream<out_stream_t> &output, int n) {
+  out_stream_t tmp = output.read();
   for (int i = 0; i < DATA_SIZE_OUT; i++) {
     #pragma HLS UNROLL
     out[(n * DATA_SIZE_OUT) + i] = tmp[i];
@@ -28,8 +28,8 @@ extern "C" {
     \param out Output Vector
 */
   void kernel_wrapper(const in_buffer_t *in, out_buffer_t *out) {
-    hls::stream<input_t> input("input");
-    hls::stream<result_t> output("output");
+    hls::stream<in_stream_t> input("input");
+    hls::stream<out_stream_t> output("output");
     #pragma HLS STREAM variable=input depth=DATA_SIZE_IN
     #pragma HLS STREAM variable=output depth=1
     
