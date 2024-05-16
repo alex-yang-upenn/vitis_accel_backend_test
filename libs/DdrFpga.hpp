2ebdc28244d17fd1e3e5459ac883127b3c96f19d
alex-yang-upenn <chukyang@sas.upenn.edu>
2024-05-16 10:59:26 -0700

    minor fixes

--- libs/DdrFpga.hpp
+++ libs/DdrFpga.hpp
@@ -20,14 +20,14 @@ class DdrFpga : public FpgaObj<V, W> {
         size_t vector_size_out_bytes = sizeof(W) * this->_kernOutputSize;
         for (int ib = 0; ib < this->_numThreads; ib++) {
             for (int ik = 0; ik < this->_numCU; ik++) {
-                cl::Buffer buffer_in_tmp (this->context, 
+                cl::Buffer buffer_in_tmp(this->context, 
                         CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
                         vector_size_in_bytes,
                         this->source_in.data() + ((ib*this->_numCU + ik) * this->_kernInputSize));
                 cl::Buffer buffer_out_tmp(this->context,
                         CL_MEM_USE_HOST_PTR | CL_MEM_EXT_PTR_XILINX | CL_MEM_WRITE_ONLY,
                         vector_size_out_bytes,
-                        this->source_hw_results.data() + ((ib*this->_numCU + ik) * this->_kernOutputSize);
+                        this->source_hw_results.data() + ((ib*this->_numCU + ik) * this->_kernOutputSize));
                 this->buffer_in.push_back(buffer_in_tmp);
                 this->buffer_out.push_back(buffer_out_tmp);
                 this->krnl_xil[ib*this->_numCU + ik].setArg(0, this->buffer_in[ib*this->_numCU + ik]);
