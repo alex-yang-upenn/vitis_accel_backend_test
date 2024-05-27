6b22ece0eb401c642f4168fc7cac9b9fa83e677c
alex-yang-upenn <chukyang@sas.upenn.edu>
2024-05-26 17:02:07 -0700

    real data

--- myproject_host_cl.cpp
+++ myproject_host_cl.cpp
@@ -30,7 +30,7 @@ int main(int argc, char **argv) {
     }
     std::string xclbinFilename = argv[1];
 
-    HbmFpga<in_buffer_t, out_buffer_t> fpga(INSTREAMSIZE, OUTSTREAMSIZE, NUM_CU, NUM_THREAD, 100); 
+    HbmFpga<in_buffer_t, out_buffer_t> fpga(INSTREAMSIZE, OUTSTREAMSIZE, NUM_CU, NUM_THREAD, 25); 
 
     std::vector<cl::Device> devices = xcl::get_xil_devices();  // Utility API that finds xilinx platforms and return a list of devices connected to Xilinx platforms
     cl::Program::Binaries bins = xcl::import_binary_file(xclbinFilename);  // Load xclbin
@@ -87,7 +87,7 @@ int main(int argc, char **argv) {
     fpga.finishRun();
 
     auto ts_end = SClock::now();
-    float throughput = (float(NUM_CU * NUM_THREAD * 100 * BATCHSIZE) /
+    float throughput = (float(NUM_CU * NUM_THREAD * 25 * BATCHSIZE) /
             float(std::chrono::duration_cast<std::chrono::nanoseconds>(ts_end - ts_start).count())) *
             1000000000.;
     std::cout << "Throughput = " << throughput <<" predictions/second\n" << std::endl;
